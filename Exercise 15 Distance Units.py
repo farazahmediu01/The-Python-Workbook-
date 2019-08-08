995da08635604a6e4d1b1c0960fad15da7e22072
Faraz Ahmed <farazahmediu01@gmail.com>
2019-08-08 15:31:04 +0500

    Exercise 15 added

---
+++ Exercise 15 Distance Units.py
@@ -0,0 +1,16 @@
+print("\n\nThe program start now...\n")
+# This program convert feet into inches, yards and miles.
+
+# User input the distance in feet.
+feet = int(input("Enter distance in feet: "))
+
+# Conversion of feet into inches, yards and miles.
+inch = feet * 12        # There are 12 inches in a foot.
+yard = feet / 3         # There are 3 feet in a yard.
+mile = feet / 5280      # There are 5280 feet in a mile.
+
+# Output of the program.
+print()
+print(feet,"\t Feet --> Inch\t",inch)
+print(feet,"\t Feet --> Yard\t",yard)
+print(feet,"\t Feet --> Mile\t",mile)
