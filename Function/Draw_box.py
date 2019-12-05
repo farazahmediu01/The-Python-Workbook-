7241b2b4dc0bb3eafa69003b0e55ae4d12e06099
Faraz Ahmed <farazahmediu01@gmail.com>
2019-12-05 15:59:56 +0500

    The Python Workbook

--- Function/Draw_box.py
+++ Function/Draw_box.py
@@ -1,7 +1,32 @@
-def draw_box():
+array = list(range(6))
+box = 0
+count_a = 0
+count_b = 0
+print(array)
+array.reverse()
+print(array)
+
+
+
+for index in range(6):
+    if array[index] > array[index + 1]:      # 5 > 4     
+        for i in range(6):
+            if array[i] > array[i + 1]:      # 5 > 4
+                box = array[i]               # box = 5
+                array[i] = array[i+1]        # array[0]   = 4
+                array[i+1] = box             # array[0+1] = 5
+                                             #[4,5,3,2,1]
+
+
+
+
+
+
+'''def draw_box():
     print("**********")
     print("*        *")
     print("*        *")
     print("**********")
 
 draw_box()
+'''
--- List/swaping_list_elements.py
+++ List/swaping_list_elements.py
@@ -1,17 +1,22 @@
 array = list(range(1,11))     # An array from 1-10.
 array.reverse()
-temp = 0
+temp  = 0
 index = 0                     # temp variable helps in swaping values of list elements.
 print("list =",array)         # printing list before changing it.
 
-for i in range(1,11):               # (loop) to make change in every element
-    if array[i] > array[index]:     # (condition) to ensure what to change, we are looking for a big value to swap with the position of small value, so we have to track both index and value 
+for i in range(1,6):               # (loop) to make change in every element
+    
+    if array[i] < array[index]:     # (condition) to ensure what to change, we are looking for a big value to swap with the position of small value, so we have to track both index and value 
+                                    # [10,9,8,7,6,5]
+                                    # [9,10,8,7,6,5]
+                                    # []
+                                    # [5,6,7,8,9,10] 
         
-       
-        temp = array[index]
-        array[index] = array[i]
+        temp = array[index]         # using temp to store list element.
+        array[index] = array[i]     # swaping 
         array[i] = temp
-        index = i 
+        index = i
+        break 
 
 print("list =",array) 
 
