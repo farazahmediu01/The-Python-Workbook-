fb680ce8eb777b2c6b1228b67e419de794e2d0e6
Faraz Ahmed <farazahmediu01@gmail.com>
2019-12-13 10:12:21 +0500

    update commit

--- Function/Exercise 85 Compute the Hypotenuse.py
+++ Function/Exercise 85 Compute the Hypotenuse.py
@@ -9,4 +9,4 @@ print('Enter two short sides of triangle to get its hypotenuse.')
 Len_1 = int(input('Enter 1st lenght: '))
 Len_2 = int(input('Enter 2nd lenght: '))
 
-print(f'\nHypotenuse = {cal_hypo(Len_1,Len_2)}')
+print(f'\nHypotenuse = {cal_hypo(Len_1,Len_2):.2f}')
