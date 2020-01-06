75b6ca55a316dc52f9bbcf645948e76999a175e6
Faraz Ahmed <farazahmediu01@gmail.com>
2020-01-06 15:14:29 +0500

    Simple Functions

--- Experiment/char_inp.py
+++ Experiment/char_inp.py
@@ -1,3 +1,15 @@
+import sys
+print()
+print("sys.implementation.name",sys.implementation.name)
+print()
+print("sys.implementation.version",sys.implementation.version)
+print()
+print("sys.executable",sys.executable)
+print()
+print(sys.path)
+
+
+'''
 name = input("Enter your name: ")              # first input
 
 age = int(input("Enter your age please:"))     # second input
@@ -7,3 +19,4 @@ dob = 2019 - age                                        # finding date of birth
 print("dob =",dob)
 print("your age is now",age,"years old")     # message
 
+'''
---
+++ Function/8-5. Cities.py
@@ -0,0 +1,6 @@
+def cities(city_name,country_name='Pakistan'):
+    print(f'The place {city_name} is in {country_name}.')
+
+cities('Karachi')
+cities('Islamabad')
+cities('Washington DC','America')
