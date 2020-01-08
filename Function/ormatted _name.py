912730f97035ea90c72e6717513d36f52ea97991
Faraz Ahmed <farazahmediu01@gmail.com>
2020-01-08 11:23:47 +0500

    functions basics

---
+++ Function/ormatted _name.py
@@ -0,0 +1,8 @@
+def formate_name(first_name, last_name):
+    full_name = f'{first_name} {last_name}'
+    #full_name = full_name.title()
+
+    return full_name.title()
+
+musician = formate_name('faraz','ahmed')
+print(musician)
