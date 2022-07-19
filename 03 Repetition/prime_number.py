9b70e2232cc07be6187062995f522b9484190671
Faraz Ahmed <farazahmediu01@gmail.com>
2022-07-19 23:25:42 +0500

    Update prime numbers

--- 03 Repetition/prime_number.py
+++ 03 Repetition/prime_number.py
@@ -1,17 +1,17 @@
 
-limit = 1000
-p = 2
-numbers = list(range(p,limit+1))
+num_a = 2
+num_z = 100
+numbers = list(range(num_a, num_z + 1))
 counter = 0
 
 while counter < len(numbers) - 1:
     
     for x in range(2, len(numbers) + 1):
-        not_a_prime_number = p * x
+        not_a_prime_number = num_a * x
         if not_a_prime_number in numbers:
             numbers.remove(not_a_prime_number)
 
     counter += 1
-    p = numbers[counter]
+    num_a = numbers[counter]
 
 print(numbers)
